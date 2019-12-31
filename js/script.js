2381729204616087202dc4a26b97c69ca6a71ce9
Vladimir <freelavlad@mail.ru>
2019-12-31 17:58:24 +0300

    if else

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-'use strict';
+let num = 150;
 
-let money = prompt('Ваш бюджет на месяц?');
-
-let date = prompt('Введите дату в формате YYYY-MM-DD');
-
-let first_q = prompt('Введите обязательную статью расходов в этом месяце');
-
-let second_q = prompt('Во сколько обойдется?');
-
-let appData = {
-    bud: money,
-    timeData: date,
-    expenses: {
-        first_q: second_q
-    },
-    optionalExpenses: {},
-    income: [],
-    savings: false
-
-};
-
-let test = appData.bud / 30;
-alert(test)
+if (num < 49){
+    console.log('Smaller')
+} else if (num > 100) {
+    console.log('Bigger')
+} else {
+    console.log('Good!')
+}
